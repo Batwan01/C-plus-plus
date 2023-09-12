7467aedbdc1c616f57537e28ba7bf711506f64b2
Batwan01 <wldhks1120@naver.com>
2023-09-12 16:45:59 +0900

    P117-9

--- C++/P117-9.cpp
+++ C++/P117-9.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-	for (int i = 0; i <= 100; i *= 10) {
-
+	for (double i = 0; i <= 100; i += 10) {
+		cout << i << '\t' << (i - 32.0) * 5.0 / 9.0 << endl;
 	}
 }
