55b3bfdec93484f05f1be309bb5141d280fbab92
Batwan01 <wldhks1120@naver.com>
2023-09-14 10:12:24 +0900

    k

---
+++ C++/practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*int i = 0;
+	for (;; ) {
+		if (i > 10)
+			break;
+		if (i < 6)
+			continue;
+		printf("%d", i);
+		i++;
+	}*/
+
+
+}
