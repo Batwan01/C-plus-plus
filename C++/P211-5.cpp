03969d1b9e9902b9735f9a915ed9acab133c899f
Batwan01 <wldhks1120@naver.com>
2023-10-14 21:35:05 +0900

    .

---
+++ C++/P211-5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+class Dice {
+public:
+	int face;
+
+	int roll();
+};
+
+int Dice::roll() {
+	face = (rand() % 6 + 1);
+	return face;
+}
+
+int main() {
+	Dice obj;
+	srand((unsigned int)time(NULL));
+
+	cout << "주사위 값=" << obj.roll() << endl;
+
+	cout << "주사위 값=" << obj.roll() << endl;
+	return 0;
+}
