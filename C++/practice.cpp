b92d550ad0867fddbc484d5e668ff4baf94209de
Batwan01 <wldhks1120@naver.com>
2023-11-27 22:04:39 +0900

    .

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -2,13 +2,20 @@
 #include <vector>
 using namespace std;
 
-class Test {
-	int x;
-public:
-	Test() { x = 0; cout << x << " "; }
-	Test(int x): x{x} {}
-}; 
 
 int main() {
-	Test a[2];
+	const int* p1;
+	int* const p2=0;
+	const int* const p3=0;
+	int a = 50;
+
+	//*p1 = 5;
+	*p1 = &a;
+	cout << *p1;
+
+	*p2 = 50;
+	//*p2 = &a;
+
+	*p3 = 50;
+	*p3 = &a;
 }
