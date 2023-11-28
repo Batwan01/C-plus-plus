2f054e114935a1c2bc1759865c7a0cff319e7bef
Batwan01 <wldhks1120@naver.com>
2023-11-28 13:40:23 +0900

    .

--- C++/P351-11.cpp
+++ C++/P351-11.cpp
@@ -32,9 +32,18 @@ int Rect::getPerimeter() {
 	const int y = this->width;
 	return (x * 2) + (y * 2);
 }
+
+bool is_equal(Rect &r1, Rect &r2) {
+	return r1.getArea() == r2.getArea(); 
+}
+
 int main() {
-	Rect* p = new Rect(3, 4);
-	(*p).print();
+	Rect* p1 = new Rect(5, 4);
+	Rect* p2 = new Rect(4, 5);
+	p1->print();
+	if (is_equal(*p1, *p2) == 1)
+		cout << "°°À½";
+	delete p1;
 	return 0;
 
 }
