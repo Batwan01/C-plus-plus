34d49ab0654b3b5330f5ef0675081f9ed10d5da3
Batwan01 <wldhks1120@naver.com>
2023-12-09 01:07:58 +0900

    .

--- C++/P431-8.cpp
+++ C++/P431-8.cpp
@@ -8,30 +8,66 @@ class Complex {
 private:
 	double real, imag;
 public:
-	Complex(double r, double i) : real(r), imag(i) {}
-	Complex operator+(const Complex& v1);
+	Complex(double r = 0, double i = 0) : real(r), imag(i) {}
+	//Complex(double r=0, double i=0) : real(r/2), imag(i) {} 5번
+	friend Complex operator+(Complex& a, Complex& b) {return Complex(a.real + b.real, a.imag + b.imag);}; //7번
+	//Complex operator+(const Complex& v1);
 	Complex operator-(const Complex& v1);
-	void print() { cout << "(" << real << "+" << imag << "i)"; }
-	string print1() { return "("+ to_string(real) + " + " + to_string(imag) + "i)"; }
+	Complex operator+=(const Complex& v1);
+	void print() { cout << "real : " << real << endl << "imag : " << imag << endl;; }
+	string print1() { return "("+ to_string((int)real) + " + " + to_string((int)imag) + "i)"; }
+	operator double() const;
+	friend ostream& operator<<(ostream& os, const Complex& v);
+	friend istream& operator>>(istream& in, Complex& v);
 };
 
+ostream& operator<<(ostream& os, const Complex& v) {
+	os << "( " << v.real << " + " << v.imag << "i )";
+	return os;
+}
+
+istream& operator>>(istream& in, Complex& v) {
+	in >> v.real >> v.imag;
+	return in;
+}
+Complex::operator double() const {
+	return sqrt(real * real + imag * imag);
+}
+
+Complex Complex::operator-(Complex const& a) {
+	Complex v;
+	v.real = this->real - a.real;
+	v.imag = this->imag - a.imag;
+	return v;
+}
+
 /*Complex Complex::operator+(const Complex& v1) {
-	Complex c(0,0);
+	Complex c;
 	c.real = this->real + v1.real;
 	c.imag = this->imag + v1.imag;
 	return c;
 }*/
 
-Complex Complex::operator+(const Complex& v1) {
-	Complex c(0, 0);
-	c.real = this->real + v1.real;
-	c.imag = this->imag + v1.imag;
-	return c;
+Complex Complex::operator+=(const Complex &v1) {
+	this->real += v1.real;
+	this->imag += v1.imag;
+	return *this;
 }
 
 int main() {
 	Complex a(1.0, 7.0), b(3.0, 8.0);
 	Complex c = a + b;
-	cout << fixed << setprecision(1) << a.print1() << " + " << b.print1() << " = " << c.print1();
+	cout <<  a.print1() << " + " << b.print1() << " = " << c.print1() << endl; //2번
+	//a.operator+(b).print(); //3번
 
+	Complex d(2, 3); //4번
+	d += a;
+	d.print();
+
+	Complex e = 6; //5번
+	e.print();
+	cout << (double)a;
+
+	cout << a;
+	
 }
