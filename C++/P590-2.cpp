88f7f8a1248aaf9b5e32c39634c24cdb1a7703ff
Batwan01 <wldhks1120@naver.com>
2023-12-10 23:46:25 +0900

    P590-2

--- C++/P590-2.cpp
+++ C++/P590-2.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main() {
 	cout << "가위, 바위, 보 게임입니다." << endl;
-
+	string word;
+	try {
+		cout << "무엇을 내시겠습니까: ";
+		cin >> word;
+	
+		if (word == "가위" || word == "바위" || word == "보")
+			cout << word << "내셨습니다.";
+		else
+			throw word;
+	}
+	catch(string e){
+		cout << "오류: 가위 바위 보 중에서만 선택하세요.";
+	}
 }
--- C++/practice.cpp
+++ C++/practice.cpp
@@ -3,32 +3,6 @@
 #include <string>
 using namespace std;
 
-class Employee {
-	string name;
-	int salary;
-public:
-	Employee(string name, int salary):name(name), salary(salary) {}
-	void setname(string name) { this->name = name; }
-	void setsalary(int salary) { this->salary = salary; }
-	string getname() { return name; }
-	int getsalary() { return salary; }
-	int computeSalary() {
-		return salary;
-	}
-};
-
-class Manager :public Employee {
-	int bonus;
-public:
-	Manager(string name, int salary, int bonus):Employee(name, salary), bonus(bonus) {}
-	void setbonus(int bonus) { this->bonus = bonus; }
-	int getbonus() { return bonus; }
-	int computeSalary() {
-		return getsalary() + bonus;
-	}
-};
-
 int main() {
-	Manager m1("±èÃ¶¼ö", 200, 100);
-	cout << "ÀüÃ¼ ¿ù±Ş: " << m1.computeSalary();
+
 }
