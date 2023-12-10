84a2a8ec2d18d971525e2dffebb4c7d8f7ceff70
Batwan01 <wldhks1120@naver.com>
2023-12-10 21:02:41 +0900

    P431-8

--- C++/P431-8.cpp
+++ C++/P431-8.cpp
@@ -1,78 +1,119 @@
 #include <iostream>
+#include <vector>
 #include <string>
-#include <iomanip> 
-
 using namespace std;
 
 class Complex {
 private:
 	double real, imag;
 public:
-	Complex(double r = 0, double i = 0) : real(r), imag(i) {}
-	//Complex(double r=0, double i=0) : real(r/2), imag(i) {} 5번
-	friend Complex operator+(Complex& a, Complex& b); //7번
-	//Complex operator+(const Complex& v1);
-	Complex operator-(const Complex& v1);
-	Complex operator+=(const Complex& v1);
-	void print() { cout << "real : " << real << endl << "imag : " << imag << endl;; }
-	string print1() { return "("+ to_string((int)real) + " + " + to_string((int)imag) + "i)"; }
-	operator double() const;
-	friend ostream& operator<<(ostream& os, const Complex& v);
-	friend istream& operator>>(istream& in, Complex& v);
+	Complex(double r = 0, double i = 0) :real(r), imag(i) {}
+	//friend Complex operator+(const Complex &p1, const Complex& p2);
+	//friend Complex operator-(const Complex &p1, const Complex& p2);
+
+	Complex operator+(const Complex& p);
+	Complex operator-(const Complex& p);
+	void set(double r, double i) { real = r; imag = i; }
+
+	void print() {
+		cout << "real : " << real << endl << "imag : " << imag << endl;
+	}
+
+	string printi() {
+		return "(" + to_string((int)real) + "+" + to_string((int)imag) + "i)";
+	}
+
+	Complex& operator+=(const Complex& p) {
+		this->real += p.real;
+		this->imag += p.imag;
+		return *this;
+	}
+	Complex& operator-=(const Complex& p);
+	operator double();
+
+	friend ostream& operator<<(ostream& os, const Complex& p) {
+		os << "real : " << p.real << endl << "imag : " << p.imag << endl;
+		return os;
+	}
+
+	friend istream& operator>>(istream& in, Complex& p) {
+		in >> p.real >> p.imag;
+		return in;
+	}
+	bool operator==(Complex& p) {
+		return (this->real == p.real && this->imag == p.imag);
+	}
+	bool operator!=(Complex& p);
 };
 
-ostream& operator<<(ostream& os, const Complex& v) {
-	os << "( " << v.real << " + " << v.imag << "i )";
-	return os;
+bool Complex::operator!=(Complex& p) {
+	return !(*this == p);
 }
 
-istream& operator>>(istream& in, Complex& v) {
-	in >> v.real >> v.imag;
-	return in;
-}
-
-Complex operator+(Complex& a, Complex& b) {
-	return Complex(a.real + b.real, a.imag + b.imag); 
-}
-
-Complex::operator double() const {
+Complex::operator double() {
 	return sqrt(real * real + imag * imag);
 }
 
-Complex Complex::operator-(Complex const& a) {
-	Complex v;
-	v.real = this->real - a.real;
-	v.imag = this->imag - a.imag;
-	return v;
+Complex& Complex::operator-=(const Complex& p) {
+	this->real -= p.real;
+	this->imag -= p.imag;
+	return *this;
+}
+Complex Complex::operator+(const Complex& p) {
+	Complex a;
+	a.real = this->real + p.real;
+	a.imag = this->imag + p.imag;
+	return a;
 }
 
-/*Complex Complex::operator+(const Complex& v1) {
-	Complex c;
-	c.real = this->real + v1.real;
-	c.imag = this->imag + v1.imag;
-	return c;
+/*Complex operator+(const Complex& p1, const Complex &p2) {
+	Complex a;
+	a.real = p1.real + p2.real;
+	a.imag = p1.imag + p2.imag;
+	return a;
 }*/
 
-Complex Complex::operator+=(const Complex &v1) {
-	this->real += v1.real;
-	this->imag += v1.imag;
-	return *this;
+/*Complex operator+(const Complex& p1, const Complex &p2) {
+	Complex a;
+	a.real = p1.real - p2.real;
+	a.imag = p1.imag - p2.imag;
+	return a;
+}*/
+
+Complex Complex::operator-(const Complex& p) {
+	Complex a;
+	a.real = this->real - p.real;
+	a.imag = this->imag - p.imag;
+	return a;
 }
 
 int main() {
-	Complex a(1.0, 7.0), b(3.0, 8.0);
+	Complex a(10, 20), b(20, 10);
 	Complex c = a + b;
-	cout <<  a.print1() << " + " << b.print1() << " = " << c.print1() << endl; //2번
-	//a.operator+(b).print(); //3번
+	c.print();
 
-	Complex d(2, 3); //4번
-	d += a;
-	d.print();
+	a.set(1.0, 7.0);
+	b.set(3.0, 8.0);
+	cout << a.printi() << " + " << b.printi() << " = " << (a + b).printi() << endl;
 
-	Complex e = 6; //5번
-	e.print();
-	cout << (double)a;
+	cout << "3번 : " << endl;
+	a.operator+(b).print();
+
+	cout << "4번 : " << endl;
+	(a += b).print();
+	(a -= b).print();
+
+	cout << endl << "5번" << endl;
+	a.print();
+	b.print();
+
+	cout << (double)a << endl; //6번
 
 	cout << a;
-	
+	cin >> a;
+	cout << a;
+
+	cout << (a == b);
+	cout << (a != b);
+
 }
--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,32 +1,119 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
-class Student {
-	string name;
+class Complex {
+private:
+	double real, imag;
 public:
-	Student(string name =""):name{name} {}
-	string getName() { return name; }
-	void setName(string name) { this->name = name; }
+	Complex(double r=0, double i=0):real(r), imag(i) {}
+	//friend Complex operator+(const Complex &p1, const Complex& p2);
+	//friend Complex operator-(const Complex &p1, const Complex& p2);
+
+	Complex operator+(const Complex& p);
+	Complex operator-(const Complex& p);
+	void set(double r, double i) { real = r; imag = i; }
+
+	void print() {
+		cout << "real : " << real << endl << "imag : " << imag << endl;
+	}
+
+	string printi() {
+		return "(" + to_string((int)real) + "+" + to_string((int)imag) + "i)";
+	}
+
+	Complex &operator+=(const Complex &p) {
+		this->real += p.real;
+		this->imag += p.imag;
+		return *this;
+	}
+	Complex& operator-=(const Complex& p);
+	operator double();
+
+	friend ostream& operator<<(ostream& os, const Complex& p) {
+		os << "real : " << p.real << endl << "imag : " << p.imag << endl;
+		return os;
+	}
+
+	friend istream& operator>>(istream& in, Complex& p) {
+		in >> p.real >> p.imag;
+		return in;
+	}
+	bool operator==(Complex& p) {
+		return (this->real == p.real && this->imag == p.imag);
+	}
+	bool operator!=(Complex& p);
 };
 
-class MyClass {
-	string className;
-	Student* p;
-	int size;
-public:
-	void setclassname(string n) { this->className = n; }
-	MyClass(Student* st, string name, int s) { p = st; className = name; size = s; }
-	void print() {
-		for (int i = 0; i < size; i++) {
-			cout << i+1 << " : " << p[i].getName() << endl;
-		}
-	}
-};
+bool Complex::operator!=(Complex& p) {
+	return !(*this == p);
+}
+
+Complex::operator double() {
+	return sqrt(real * real + imag * imag);
+}
+
+Complex &Complex::operator-=(const Complex& p) {
+	this->real -= p.real;
+	this->imag -= p.imag;
+	return *this;
+}
+Complex Complex::operator+(const Complex& p) {
+	Complex a;
+	a.real = this->real + p.real;
+	a.imag = this->imag + p.imag;
+	return a;
+}
+
+/*Complex operator+(const Complex& p1, const Complex &p2) {
+	Complex a;
+	a.real = p1.real + p2.real;
+	a.imag = p1.imag + p2.imag;
+	return a;
+}*/
+
+/*Complex operator+(const Complex& p1, const Complex &p2) {
+	Complex a;
+	a.real = p1.real - p2.real;
+	a.imag = p1.imag - p2.imag;
+	return a;
+}*/
+
+Complex Complex::operator-(const Complex& p) {
+	Complex a;
+	a.real = this->real - p.real;
+	a.imag = this->imag - p.imag;
+	return a;
+}
 
 int main() {
-	Student* st = new Student[3]{ Student("jiwan"), Student("jiw"), Student("asd") };
-	MyClass clas(st, "specail", 3);
-	clas.print();
-	delete[] st;
+	Complex a(10, 20), b(20, 10);
+	Complex c = a + b;
+	c.print();
+
+	a.set(1.0, 7.0);
+	b.set(3.0, 8.0);
+	cout << a.printi() << " + " << b.printi() << " = " << (a + b).printi() << endl;
+	
+	cout << "3번 : " << endl;
+	a.operator+(b).print();
+
+	cout << "4번 : " << endl;
+	(a += b).print();
+	(a -= b).print();
+
+	cout << endl << "5번" << endl;
+	a.print();
+	b.print();
+
+	cout << (double)a << endl; //6번
+
+	cout << a;
+	cin >> a;
+	cout << a;
+
+	cout << (a == b);
+	cout << (a != b);
+
 }
