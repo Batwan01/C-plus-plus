16452c9bbc6e1ddf17e319474da75f9895919dfd
Batwan01 <wldhks1120@naver.com>
2023-12-10 21:47:28 +0900

    P473-8

--- C++/P473-8.cpp
+++ C++/P473-8.cpp
@@ -1,42 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
-
 class Human {
-protected:
 	string name;
 	int age;
 public:
-	Human(string name, int age) {
-		this->name = name;
-		this->age = age;
-	}
-
-	void setname(string name) {
-		this->name = name;
-	}
-
-	void setage(int age) {
-		this->age = age;
-	}
-
-	void print() {
-		cout << "이름 : " << name << endl << "나이 : " << age << endl << endl;
-	}
+	Human(string name, int age) :name(name), age(age) {}
+	void setname(string name) { this->name = name; }
+	void srtage(int age) { this->age = age; }
+	string getname() { return name; }
+	int getage() { return age; }
 };
 
-class Student:public Human {
+class Student :public Human {
 	string major;
 public:
+	Student(string name, int age, string major) : Human(name, age), major(major) {}
 	void print() {
-		cout << "이름 : " << name << endl << "나이 : " << age << endl << "전공 : " << major << endl << endl;
+		cout << "이름 : " << getname() << endl << "나이 : " << getage() << endl << "전공 : " << major << endl << endl;
 	}
-	Student(string name, int age, string major) :Human(name, age), major{ major } {}
 };
-
 int main() {
-	Student h1("춘향", 18, "컴"), h2("몽룡", 21, "소"), h3("사또", 50, "멀");
-	h1.print();
-	h1.Human::print();
-	h2.print();
-	h3.print();
+	Student* h = new Student[3]{ Student("춘향", 18,"컴공") , Student("몽룡",21, "간호"), Student("사또", 50, "유교") };
+	for (int i = 0; i < 3; i++)
+		h[i].print();
 }
--- C++/practice.cpp
+++ C++/practice.cpp
@@ -2,118 +2,27 @@
 #include <vector>
 #include <string>
 using namespace std;
-
-class Complex {
-private:
-	double real, imag;
+class Human {
+	string name;
+	int age;
 public:
-	Complex(double r=0, double i=0):real(r), imag(i) {}
-	//friend Complex operator+(const Complex &p1, const Complex& p2);
-	//friend Complex operator-(const Complex &p1, const Complex& p2);
-
-	Complex operator+(const Complex& p);
-	Complex operator-(const Complex& p);
-	void set(double r, double i) { real = r; imag = i; }
-
-	void print() {
-		cout << "real : " << real << endl << "imag : " << imag << endl;
-	}
-
-	string printi() {
-		return "(" + to_string((int)real) + "+" + to_string((int)imag) + "i)";
-	}
-
-	Complex &operator+=(const Complex &p) {
-		this->real += p.real;
-		this->imag += p.imag;
-		return *this;
-	}
-	Complex& operator-=(const Complex& p);
-	operator double();
-
-	friend ostream& operator<<(ostream& os, const Complex& p) {
-		os << "real : " << p.real << endl << "imag : " << p.imag << endl;
-		return os;
-	}
-
-	friend istream& operator>>(istream& in, Complex& p) {
-		in >> p.real >> p.imag;
-		return in;
-	}
-	bool operator==(Complex& p) {
-		return (this->real == p.real && this->imag == p.imag);
-	}
-	bool operator!=(Complex& p);
+	Human(string name, int age):name(name), age(age) {}
+	void setname(string name) { this->name = name; }
+	void srtage(int age) { this->age = age; }
+	string getname() { return name; }
+	int getage() { return age; }
 };
 
-bool Complex::operator!=(Complex& p) {
-	return !(*this == p);
-}
-
-Complex::operator double() {
-	return sqrt(real * real + imag * imag);
-}
-
-Complex &Complex::operator-=(const Complex& p) {
-	this->real -= p.real;
-	this->imag -= p.imag;
-	return *this;
-}
-Complex Complex::operator+(const Complex& p) {
-	Complex a;
-	a.real = this->real + p.real;
-	a.imag = this->imag + p.imag;
-	return a;
-}
-
-/*Complex operator+(const Complex& p1, const Complex &p2) {
-	Complex a;
-	a.real = p1.real + p2.real;
-	a.imag = p1.imag + p2.imag;
-	return a;
-}*/
-
-/*Complex operator+(const Complex& p1, const Complex &p2) {
-	Complex a;
-	a.real = p1.real - p2.real;
-	a.imag = p1.imag - p2.imag;
-	return a;
-}*/
-
-Complex Complex::operator-(const Complex& p) {
-	Complex a;
-	a.real = this->real - p.real;
-	a.imag = this->imag - p.imag;
-	return a;
-}
-
+class Student :public Human {
+	string major;
+public:
+	Student(string name, int age, string major): Human(name, age), major(major) {}
+	void print() {
+		cout << "이름 : " << getname() << endl << "나이 : " << getage() << endl << "전공 : " << major << endl << endl;
+	}
+};
 int main() {
-	Complex a(10, 20), b(20, 10);
-	Complex c = a + b;
-	c.print();
-
-	a.set(1.0, 7.0);
-	b.set(3.0, 8.0);
-	cout << a.printi() << " + " << b.printi() << " = " << (a + b).printi() << endl;
-	
-	cout << "3번 : " << endl;
-	a.operator+(b).print();
-
-	cout << "4번 : " << endl;
-	(a += b).print();
-	(a -= b).print();
-
-	cout << endl << "5번" << endl;
-	a.print();
-	b.print();
-
-	cout << (double)a << endl; //6번
-
-	cout << a;
-	cin >> a;
-	cout << a;
-
-	cout << (a == b);
-	cout << (a != b);
-
+	Student  *h = new Student[3]{ Student("춘향", 18,"컴공") , Student("몽룡",21, "간호"), Student("사또", 50, "유교")};
+	for (int i = 0; i < 3; i++)
+		h[i].print();
 }
