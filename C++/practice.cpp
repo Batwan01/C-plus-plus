44bfd86584bb7a1f210d1742a07abe2eaae44091
Batwan01 <wldhks1120@naver.com>
2023-12-12 22:50:22 +0900

    .

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -14,11 +14,16 @@ public:
 	void setheight(int height) { this->height = height; }
 };
 
+bool is_equal(Rect &r1, Rect& r2) {
+	return (r1.getheight() == r2.getheight() && r1.getwidth() == r2.getwidth());
+}
+
 int main() {
-	Rect r1;
+	Rect r1, r2;
 	r1.setheight(5);
 	r1.setwidth(4);
-
-	cout << r1.getArea() << endl << r1.getPerimeter();
+	r2.setheight(5);
+	r2.setwidth(4);
+	cout << r1.getArea() << endl << r1.getPerimeter() << is_equal(r1,r2);
 
 }
