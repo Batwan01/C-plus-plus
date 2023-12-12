b178b891241ae1a88c72c3d26b6da3399b80148e
Batwan01 <wldhks1120@naver.com>
2023-12-12 22:42:03 +0900

    .

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -3,9 +3,22 @@
 #include <string>
 using namespace std;
 
+class Rect {
+	int width, height;
+public:
+	int getArea() { return width * height; }
+	int getPerimeter() { return 2 * width + 2 * height; }
+	int getwidth() { return width; }
+	int getheight() { return height; }
+	void setwidth(int width) { this->width = width; }
+	void setheight(int height) { this->height = height; }
+};
+
 int main() {
-	int a;
-	int b;
-	cin >> hex >> a >> b;
-	cout <<hex<< a - b;
+	Rect r1;
+	r1.setheight(5);
+	r1.setwidth(4);
+
+	cout << r1.getArea() << endl << r1.getPerimeter();
+
 }
