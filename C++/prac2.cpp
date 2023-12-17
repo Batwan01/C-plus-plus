fb6109168879a406c2739865694eb320dcaa3964
Batwan01 <wldhks1120@naver.com>
2023-12-17 14:56:02 +0900

    nope

--- C++/prac2.cpp
+++ C++/prac2.cpp
@@ -5,27 +5,27 @@ class HomeAppliance {
 	int price;
 	virtual double getPrice() = 0;
 public:
-	//HomeAppliance(int price) :price(price) {}
+	HomeAppliance(int price) :price(price) {}
 	int get_price() { return price; }
 	virtual void print() = 0;
 };
 
 class Television :public HomeAppliance {
 public:
-	//Television(int price) : HomeAppliance(price) {}
+	Television(int price) : HomeAppliance(price) {}
 	double getPrice() { return get_price() * 0.9; }
 	void print() { cout << "Tv : " << getPrice() << endl; }
 };
 
 class Refrigerator :public HomeAppliance {
 public:
-	//Refrigerator(int price) : HomeAppliance(price) {}
+	Refrigerator(int price) : HomeAppliance(price) {}
 	double getPrice() { return get_price() * 0.95; }
 	void print() { cout << "Refi : " << getPrice() << endl; }
 };
 
 int main() {
-/*	HomeAppliance* home[4];
+	HomeAppliance* home[4];
 	home[0] = new Television(1000000);
 	home[1] = new Refrigerator(1000000);
 	home[2] = new Television(200000);
@@ -33,8 +33,8 @@ int main() {
 
 	for (int i = 0; i < 4; i++) {
 		home[i]->print();
-	}*/
-	HomeAppliance* p = new Television;
-
+	}
+	
+	HomeAppliance* nhome = new HomeAppliance[2](Television(100000), Refrigerator(52222));
 
 }
